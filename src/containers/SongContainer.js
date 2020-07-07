149bf7336a573213caca3ee9cd1ce82d8eae6a86
CameronTait <cameron-tait@hotmail.com>
2020-07-07 23:08:05 +0100

    worked on song container file

---
+++ src/containers/SongContainer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SongSelector from '../components/SongSelector';
+import SongDetail from '../components/SongDetail';
+
+class SongContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+      selectedSongId: ''
+    };
+    this.handleSongSelected = this.handleSongSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const url = 'https://rss.itunes.apple.com/api/v1/us/itunes-music/top-songs/all/100/explicit.json';
+
+    fetch(url)
+      .then(res => res.json())
+      .then(songs => this.setState({ songs: songs }))
+      .catch(err => console.error);
+  }
+
+  handleSongSelected(id) {
+    this.setState({ selectedSongId: id })
+  }
+
+  render() {
+    const selectedSong = this.state.songs.find(song => song.id === this.state.selectedSongId)
+    
+    return (
+      <div>
+        <h2>Song Container</h2>
+        <SongSelector songs={this.state.songs} onSongSelected={this.handleSongSelected} />
+        <SongDetail song={selectedSong} />
+      </div>
+    );
+  }
+}
+
+export default SongContainer;
